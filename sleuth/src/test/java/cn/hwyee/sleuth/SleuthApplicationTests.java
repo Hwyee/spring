ae0b9d0504f51ea5a5ad387605c508499272fe67
hwyee <hwyee@foxmail.com>
2023-03-19 14:26:50 +0800

    add module cache common consumer gateway jpa provider seata sentinel sleuth webflow

---
+++ sleuth/src/test/java/cn/hwyee/sleuth/SleuthApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hwyee.sleuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
