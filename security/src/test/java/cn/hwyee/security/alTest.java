f302ab4ebc2ccba1eb07e3c472a331fd934c883a
hwyee <hwyee@foxmail.com>
2023-03-23 17:27:29 +0800

    d3

--- security/src/test/java/cn/hwyee/security/alTest.java
+++ security/src/test/java/cn/hwyee/security/alTest.java
@@ -1,8 +1,11 @@
 package cn.hwyee.security;
 
+import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.context.TestPropertySource;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -12,7 +15,9 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.Stack;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
+
 
 public class alTest {
     @Test
@@ -159,27 +164,29 @@ public class alTest {
             return Arrays.stream(ele).min().getAsInt();
         }
     }
+
     @Test
-    public void fib(){
+    public void fib() {
         Solution solution = new Solution();
         System.out.println(solution.fib(5));
     }
+
     class Solution {
 
         public int fib(int n) {
             //动态规划
 
-                    if (n < 2) {
-                        return n;
-                    }
-                    int p = 0, q = 0, r = 1;
-                    for (int i = 2; i <= n; ++i) {
-                        p = q;
-                        q = r;
-                        r = p + q;
-                    }
-                    return r;
-        //矩阵快速幂
+            if (n < 2) {
+                return n;
+            }
+            int p = 0, q = 0, r = 1;
+            for (int i = 2; i <= n; ++i) {
+                p = q;
+                q = r;
+                r = p + q;
+            }
+            return r;
+            //矩阵快速幂
 
 //                    if (n < 2) {
 //                        return n;
@@ -189,7 +196,7 @@ public class alTest {
 //                    return res[0][0];
 
 
-        //通项公式
+            //通项公式
 //            double sqrt5 = Math.sqrt(5);
 //            double fibN = Math.pow((1 + sqrt5) / 2, n) - Math.pow((1 - sqrt5) / 2, n);
 //            return (int) Math.round(fibN / sqrt5);
@@ -205,10 +212,11 @@ public class alTest {
             //chao出时间限制
             if (n == 0)
                 return 0;
-            if (n == 1 ) return  1;
-            if (n == 2 ) return 1;
-            return tribonacci(n - 1) + tribonacci(n-2) + tribonacci(n-3);
+            if (n == 1) return 1;
+            if (n == 2) return 1;
+            return tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
         }
+
         public int[][] pow(int[][] a, int n) {
             int[][] ret = {{1, 0}, {0, 1}};
             while (n > 0) {
@@ -230,5 +238,234 @@ public class alTest {
             }
             return c;
         }
+    }
+
+
+    @Test
+    public void d2t() {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        node1.next=node2;
+        node2.next=node3;
+        node1.random = node3;
+        node2.random = null;
+        node3.random = node1;
+        D2 d2 = new D2();
+        Node node = d2.copyRandomList(node1);
+        System.out.println(""+node.val+node.next.val+(node.random==null?"Null":node.random.val));
+    }
+
+
+        class D2 {
+            //1输入链表头节点,从尾到头打印节点的值,返回数组
+
+            public  int[] reversePrint(ListNode head) {
+                if (head == null) {
+                    return new int[0];
+                }
+                Stack<Integer> listNodes = new Stack<>();
+                listNodes.push(head.val);
+                while (head.next != null) {
+                    head = head.next;
+                    listNodes.push(head.val);
+                }
+                int[] a = new int[listNodes.size()];
+                for (int i = 0; i < a.length; i++) {
+                    a[i] = listNodes.pop();
+                }
+                return a;
+            }
+
+            //输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+            public  ListNode reversePrintNode(ListNode head) {
+
+                ListNode pre = null;
+                ListNode now = head;
+                while (now != null) {
+                    ListNode temp = now.next;
+                    now.next = pre;
+
+                    pre = now;
+                    now = temp;
+                }
+                return pre;
+            }
+
+            //请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，
+            // 每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+            public Node copyRandomList(Node head) {
+                if (head == null){
+                    return null;
+                }
+                Node node = new Node(head.val);
+                Node pre = node;
+                List<Node> oldL = new ArrayList<Node>();
+                List<Node> newL = new ArrayList<Node>();
+                oldL.add(head);
+                newL.add(node);
+                while (head.next != null){
+                    oldL.add(head.next);
+                    Node n = new Node(head.next.val);
+                    newL.add(n);
+                    pre.next = n;
+                    pre = n;
+                    head = head.next;
+                }
+                AtomicInteger atomicInteger = new AtomicInteger(0);
+                newL.forEach( t -> {
+                    int index = getIndex(oldL, oldL.get(atomicInteger.getAndAdd(1)).random);
+                    if (index == -1){
+                        t.random = null;
+                    } else {
+                        t.random=newL.get(index);
+                    }
+                });
+                return newL.get(0);
+            }
+
+            private int getIndex(List<Node> old,Node node){
+                if (node == null) return -1;
+                for (int i = 0; i < old.size(); i++) {
+                    if (node == old.get(i))
+                        return i;
+                }
+                return -1;
+            }
+
+        }
+
+        @Test
+    public void d3T(){
+        char a = '1';
+        char b = 97;
+        int c = 'c';
+            System.out.println(a +""+ b);
+        }
+
+    }
+    class d3{
+       // 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+       public String replaceSpace(String s) {
+           char[] chars = s.toCharArray();
+           StringBuilder stringBuilder = new StringBuilder();
+           for (char aChar : chars) {
+               if (aChar == ' '){
+                   stringBuilder.append("%20");
+               }else {
+                   stringBuilder.append(aChar);
+               }
+           }
+//           while (s.contains(" ")){
+//              int i = s.indexOf(" ");
+//              s = s.substring(0,i) + "%20" + s.substring(i+1);
+//          }
+        return stringBuilder.toString();
+       }
+       //字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
+        // 请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，
+        // 该函数将返回左旋转两位得到的结果"cdefgab"。
+       public String reverseLeftWords(String s, int n) {
+           StringBuilder left = new StringBuilder();
+           StringBuilder right = new StringBuilder();
+           char[] chars = s.toCharArray();
+           for (int i = 0; i < chars.length; i++) {
+               if (i < n){
+                   right.append(chars[i]);
+               }else{
+                   left.append(chars[i]);
+               }
+           }
+           return left.append(right).toString();
+
+       }
+
+       //给定两个大小分别为 m 和 n 的正序（从小到大）数组nums1 和nums2。请你找出并返回这两个正序数组的 中位数 。
+       public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+//           int[] sum =  new int[nums1.length+nums2.length];
+//           System.arraycopy(nums1,0,sum,0,nums1.length);
+//           System.arraycopy(nums2,0,sum,nums1.length,nums2.length);
+//           Arrays.sort(sum);
+//
+//           if (sum.length%2==0){
+//              return  (sum[sum.length/2 - 1] + sum[sum.length/2])/2.0;
+//           }
+//           return (double) sum[sum.length / 2];
+//
+           public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+               int[] nums;
+               int m = nums1.length;
+               int n = nums2.length;
+               nums = new int[m + n];
+               if (m == 0) {
+                   if (n % 2 == 0) {
+                       return (nums2[n / 2 - 1] + nums2[n / 2]) / 2.0;
+                   } else {
+
+                       return nums2[n / 2];
+                   }
+               }
+               if (n == 0) {
+                   if (m % 2 == 0) {
+                       return (nums1[m / 2 - 1] + nums1[m / 2]) / 2.0;
+                   } else {
+                       return nums1[m / 2];
+                   }
+               }
+
+               int count = 0;
+               int i = 0, j = 0;
+               while (count != (m + n)) {
+                   if (i == m) {
+                       while (j != n) {
+                           nums[count++] = nums2[j++];
+                       }
+                       break;
+                   }
+                   if (j == n) {
+                       while (i != m) {
+                           nums[count++] = nums1[i++];
+                       }
+                       break;
+                   }
+
+                   if (nums1[i] < nums2[j]) {
+                       nums[count++] = nums1[i++];
+                   } else {
+                       nums[count++] = nums2[j++];
+                   }
+               }
+
+               if (count % 2 == 0) {
+                   return (nums[count / 2 - 1] + nums[count / 2]) / 2.0;
+               } else {
+                   return nums[count / 2];
+               }
+           }
+
+
+       }
+
+
+    }
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
     }
 }
