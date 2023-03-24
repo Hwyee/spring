36cfbbfe6f323964a2aabd448962403c255e9ccc
hwyee <hwyee@foxmail.com>
2023-03-24 17:29:11 +0800

    d4 no

--- security/src/test/java/cn/hwyee/security/alTest.java
+++ security/src/test/java/cn/hwyee/security/alTest.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -246,142 +247,152 @@ public class alTest {
         Node node1 = new Node(1);
         Node node2 = new Node(2);
         Node node3 = new Node(3);
-        node1.next=node2;
-        node2.next=node3;
+        node1.next = node2;
+        node2.next = node3;
         node1.random = node3;
         node2.random = null;
         node3.random = node1;
         D2 d2 = new D2();
         Node node = d2.copyRandomList(node1);
-        System.out.println(""+node.val+node.next.val+(node.random==null?"Null":node.random.val));
+        System.out.println("" + node.val + node.next.val + (node.random == null ? "Null" : node.random.val));
     }
 
 
-        class D2 {
-            //1输入链表头节点,从尾到头打印节点的值,返回数组
+    class D2 {
+        //1输入链表头节点,从尾到头打印节点的值,返回数组
 
-            public  int[] reversePrint(ListNode head) {
-                if (head == null) {
-                    return new int[0];
-                }
-                Stack<Integer> listNodes = new Stack<>();
+        public int[] reversePrint(ListNode head) {
+            if (head == null) {
+                return new int[0];
+            }
+            Stack<Integer> listNodes = new Stack<>();
+            listNodes.push(head.val);
+            while (head.next != null) {
+                head = head.next;
                 listNodes.push(head.val);
-                while (head.next != null) {
-                    head = head.next;
-                    listNodes.push(head.val);
-                }
-                int[] a = new int[listNodes.size()];
-                for (int i = 0; i < a.length; i++) {
-                    a[i] = listNodes.pop();
-                }
-                return a;
             }
-
-            //输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
-            public  ListNode reversePrintNode(ListNode head) {
-
-                ListNode pre = null;
-                ListNode now = head;
-                while (now != null) {
-                    ListNode temp = now.next;
-                    now.next = pre;
-
-                    pre = now;
-                    now = temp;
-                }
-                return pre;
+            int[] a = new int[listNodes.size()];
+            for (int i = 0; i < a.length; i++) {
+                a[i] = listNodes.pop();
             }
-
-            //请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，
-            // 每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
-            public Node copyRandomList(Node head) {
-                if (head == null){
-                    return null;
-                }
-                Node node = new Node(head.val);
-                Node pre = node;
-                List<Node> oldL = new ArrayList<Node>();
-                List<Node> newL = new ArrayList<Node>();
-                oldL.add(head);
-                newL.add(node);
-                while (head.next != null){
-                    oldL.add(head.next);
-                    Node n = new Node(head.next.val);
-                    newL.add(n);
-                    pre.next = n;
-                    pre = n;
-                    head = head.next;
-                }
-                AtomicInteger atomicInteger = new AtomicInteger(0);
-                newL.forEach( t -> {
-                    int index = getIndex(oldL, oldL.get(atomicInteger.getAndAdd(1)).random);
-                    if (index == -1){
-                        t.random = null;
-                    } else {
-                        t.random=newL.get(index);
-                    }
-                });
-                return newL.get(0);
-            }
-
-            private int getIndex(List<Node> old,Node node){
-                if (node == null) return -1;
-                for (int i = 0; i < old.size(); i++) {
-                    if (node == old.get(i))
-                        return i;
-                }
-                return -1;
-            }
-
+            return a;
         }
 
-        @Test
-    public void d3T(){
+        //输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+        public ListNode reversePrintNode(ListNode head) {
+
+            ListNode pre = null;
+            ListNode now = head;
+            while (now != null) {
+                ListNode temp = now.next;
+                now.next = pre;
+
+                pre = now;
+                now = temp;
+            }
+            return pre;
+        }
+
+        //请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，
+        // 每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+        public Node copyRandomList(Node head) {
+            if (head == null) {
+                return null;
+            }
+            Node node = new Node(head.val);
+            Node pre = node;
+            List<Node> oldL = new ArrayList<Node>();
+            List<Node> newL = new ArrayList<Node>();
+            oldL.add(head);
+            newL.add(node);
+            while (head.next != null) {
+                oldL.add(head.next);
+                Node n = new Node(head.next.val);
+                newL.add(n);
+                pre.next = n;
+                pre = n;
+                head = head.next;
+            }
+            AtomicInteger atomicInteger = new AtomicInteger(0);
+            newL.forEach(t -> {
+                int index = getIndex(oldL, oldL.get(atomicInteger.getAndAdd(1)).random);
+                if (index == -1) {
+                    t.random = null;
+                } else {
+                    t.random = newL.get(index);
+                }
+            });
+            return newL.get(0);
+        }
+
+        private int getIndex(List<Node> old, Node node) {
+            if (node == null) return -1;
+            for (int i = 0; i < old.size(); i++) {
+                if (node == old.get(i))
+                    return i;
+            }
+            return -1;
+        }
+
+    }
+
+    @Test
+    public void d3T() {
         char a = '1';
         char b = 97;
         int c = 'c';
-            System.out.println(a +""+ b);
-        }
-
+        System.out.println(a + "" + b);
     }
-    class d3{
-       // 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
-       public String replaceSpace(String s) {
-           char[] chars = s.toCharArray();
-           StringBuilder stringBuilder = new StringBuilder();
-           for (char aChar : chars) {
-               if (aChar == ' '){
-                   stringBuilder.append("%20");
-               }else {
-                   stringBuilder.append(aChar);
-               }
-           }
+
+    @Test
+    public void d4T() {
+        int[] a = new int[]{5,7,7,8,8,10};
+        D4 d4 = new D4();
+        int search = d4.search(a, 8);
+        System.out.println(search);
+    }
+
+}
+
+class d3 {
+    // 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+    public String replaceSpace(String s) {
+        char[] chars = s.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char aChar : chars) {
+            if (aChar == ' ') {
+                stringBuilder.append("%20");
+            } else {
+                stringBuilder.append(aChar);
+            }
+        }
 //           while (s.contains(" ")){
 //              int i = s.indexOf(" ");
 //              s = s.substring(0,i) + "%20" + s.substring(i+1);
 //          }
         return stringBuilder.toString();
-       }
-       //字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
-        // 请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，
-        // 该函数将返回左旋转两位得到的结果"cdefgab"。
-       public String reverseLeftWords(String s, int n) {
-           StringBuilder left = new StringBuilder();
-           StringBuilder right = new StringBuilder();
-           char[] chars = s.toCharArray();
-           for (int i = 0; i < chars.length; i++) {
-               if (i < n){
-                   right.append(chars[i]);
-               }else{
-                   left.append(chars[i]);
-               }
-           }
-           return left.append(right).toString();
+    }
 
-       }
+    //字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。
+    // 请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，
+    // 该函数将返回左旋转两位得到的结果"cdefgab"。
+    public String reverseLeftWords(String s, int n) {
+        StringBuilder left = new StringBuilder();
+        StringBuilder right = new StringBuilder();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (i < n) {
+                right.append(chars[i]);
+            } else {
+                left.append(chars[i]);
+            }
+        }
+        return left.append(right).toString();
 
-       //给定两个大小分别为 m 和 n 的正序（从小到大）数组nums1 和nums2。请你找出并返回这两个正序数组的 中位数 。
-       public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    }
+
+    //给定两个大小分别为 m 和 n 的正序（从小到大）数组nums1 和nums2。请你找出并返回这两个正序数组的 中位数 。
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 //           int[] sum =  new int[nums1.length+nums2.length];
 //           System.arraycopy(nums1,0,sum,0,nums1.length);
 //           System.arraycopy(nums2,0,sum,nums1.length,nums2.length);
@@ -392,62 +403,147 @@ public class alTest {
 //           }
 //           return (double) sum[sum.length / 2];
 //
-           public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-               int[] nums;
-               int m = nums1.length;
-               int n = nums2.length;
-               nums = new int[m + n];
-               if (m == 0) {
-                   if (n % 2 == 0) {
-                       return (nums2[n / 2 - 1] + nums2[n / 2]) / 2.0;
-                   } else {
+//               int[] nums;
+//               int m = nums1.length;
+//               int n = nums2.length;
+//               nums = new int[m + n];
+//               if (m == 0) {
+//                   if (n % 2 == 0) {
+//                       return (nums2[n / 2 - 1] + nums2[n / 2]) / 2.0;
+//                   } else {
+//
+//                       return nums2[n / 2];
+//                   }
+//               }
+//               if (n == 0) {
+//                   if (m % 2 == 0) {
+//                       return (nums1[m / 2 - 1] + nums1[m / 2]) / 2.0;
+//                   } else {
+//                       return nums1[m / 2];
+//                   }
+//               }
+//
+//               int count = 0;
+//               int i = 0, j = 0;
+//               while (count != (m + n)) {
+//                   if (i == m) {
+//                       while (j != n) {
+//                           nums[count++] = nums2[j++];
+//                       }
+//                       break;
+//                   }
+//                   if (j == n) {
+//                       while (i != m) {
+//                           nums[count++] = nums1[i++];
+//                       }
+//                       break;
+//                   }
+//
+//                   if (nums1[i] < nums2[j]) {
+//                       nums[count++] = nums1[i++];
+//                   } else {
+//                       nums[count++] = nums2[j++];
+//                   }
+//               }
+//
+//               if (count % 2 == 0) {
+//                   return (nums[count / 2 - 1] + nums[count / 2]) / 2.0;
+//               } else {
+//                   return nums[count / 2];
+//               }
+//           }
 
-                       return nums2[n / 2];
-                   }
-               }
-               if (n == 0) {
-                   if (m % 2 == 0) {
-                       return (nums1[m / 2 - 1] + nums1[m / 2]) / 2.0;
-                   } else {
-                       return nums1[m / 2];
-                   }
-               }
-
-               int count = 0;
-               int i = 0, j = 0;
-               while (count != (m + n)) {
-                   if (i == m) {
-                       while (j != n) {
-                           nums[count++] = nums2[j++];
-                       }
-                       break;
-                   }
-                   if (j == n) {
-                       while (i != m) {
-                           nums[count++] = nums1[i++];
-                       }
-                       break;
-                   }
-
-                   if (nums1[i] < nums2[j]) {
-                       nums[count++] = nums1[i++];
-                   } else {
-                       nums[count++] = nums2[j++];
-                   }
-               }
-
-               if (count % 2 == 0) {
-                   return (nums[count / 2 - 1] + nums[count / 2]) / 2.0;
-               } else {
-                   return nums[count / 2];
-               }
-           }
-
-
-       }
-
-
+        int m = nums1.length;
+        int n = nums2.length;
+        int len = m + n;//两个数组总长度
+        int left = -1, right = -1;
+        int aStart = 0, bStart = 0;
+        for (int i = 0; i <= len / 2; i++) {
+            left = right;
+            if (aStart < m && (bStart >= n || nums1[aStart] < nums2[bStart])) {
+                right = nums1[aStart++];
+            } else {
+                right = nums2[bStart++];
+            }
+        }
+        if ((len & 1) == 0)//偶数
+            return (left + right) / 2.0;
+        else//奇数
+            return right;
     }
+
+}
+
+class D4 {
+    //在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，
+    // 但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+    public int findRepeatNumber(int[] nums) {
+        int i = 1;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+            i++;
+            if (i != set.size()) {
+                return num;
+            }
+        }
+        return 0;
+    }
+
+    //统计一个数字在排序数组中出现的次数。
+    public int search(int[] nums, int target) {
+        if (nums.length == 1) return 0;
+        int start = -1;
+        int end = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (start != -1 && end !=-1){
+                break;
+            }
+            if (nums[i] == target && start == -1) {
+                start = i;
+            }
+            if (nums[nums.length - i - 1] == target && end == -1) {
+                end = nums.length - i ;
+            }
+        }
+        if (start == -1 || end == -1) return 0;
+        return end - start + 1;
+    }
+    public int search1(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return rightIdx - leftIdx + 1;
+        }
+        return 0;
+    }
+
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+    //一个长度为n-1的递增排序数组中的所有数字都是唯一的，并且每个数字都在范围0～n-1之内。
+    // 在范围0～n-1内的n个数字中有且只有一个数字不在该数组中，请找出这个数字。
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int a = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == i++);
+                i++;
+        }
+        return 0;
+    }
+
+}
 
 class ListNode {
     int val;
