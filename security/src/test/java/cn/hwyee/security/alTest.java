316ade7254185f08c8ee110c1ceab338d8ca2808
hwyee <hwyee@foxmail.com>
2023-03-27 17:28:24 +0800

    d5 1

--- security/src/test/java/cn/hwyee/security/alTest.java
+++ security/src/test/java/cn/hwyee/security/alTest.java
@@ -558,6 +558,32 @@ class D4 {
     }
 
 }
+class D5{
+    //在一个 n * m 的二维数组中，每一行都按照从左到右非递减的顺序排序，
+    // 每一列都按照从上到下非递减的顺序排序。请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix==null || matrix.length==0 || matrix[0].length==0) return false;
+        int m = matrix[0].length;
+        int n = matrix.length;
+        int a = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i][m-1] >= target){
+                a = i;
+                break;
+            }
+        }
+        while (a<matrix.length && matrix[a][0] <= target ){
+            for (int i = 0; i < m; i++) {
+                if (matrix[a][i] == target){
+                    return true;
+                }
+            }
+            a++;
+        }
+
+        return false;
+    }
+}
 
 class ListNode {
     int val;
