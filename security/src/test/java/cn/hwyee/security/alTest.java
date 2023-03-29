2343b8c779c3de371e0ede2a38ed30775ae532a7
hwyee <hwyee@foxmail.com>
2023-03-29 17:40:07 +0800

    D5 2

--- security/src/test/java/cn/hwyee/security/alTest.java
+++ security/src/test/java/cn/hwyee/security/alTest.java
@@ -583,6 +583,39 @@ class D5{
 
         return false;
     }
+
+    //把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+    //
+    //给你一个可能存在重复元素值的数组numbers，它原来是一个升序排列的数组，并按上述情形进行了一次旋转。
+    // 请返回旋转数组的最小元素。例如，数组[3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为 1。
+    //
+    //注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+    public int minArray(int[] numbers) {
+        if (numbers[numbers.length-1]>numbers[0]){
+            return numbers[0];
+        }
+        int start = 0;
+        int end = numbers.length - 1 ;
+        int mid = 0;
+        while (start<end){
+            mid = start +  (end - start)/2;// 等同于(start + start) / 2,为了防止int溢出,所以不用这种写法。
+            if (numbers[mid] < numbers[end]){
+                end = mid;
+            } else if (numbers[mid] > numbers[end]){
+                start = mid+1;
+            }
+            else {
+                end -=1;
+            }
+        }
+        return numbers[start];
+    }
+    //在字符串 s 中找出第一个只出现一次的字符。如果没有，返回一个单空格。 s 只包含小写字母。
+    public char firstUniqChar(String s) {
+
+        return 1;
+    }
+
 }
 
 class ListNode {
