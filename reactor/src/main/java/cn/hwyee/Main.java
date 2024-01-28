8cf4834f4bbde9d7ff21a1164860e1c73962b9d7
hwyee <hwyee@foxmail.com>
2024-01-29 00:10:54 +0800

    1.java 9 reactor-stream example. 2.add Reactor frame dependency.

--- reactor/src/main/java/cn/hwyee/Main.java
+++ reactor/src/main/java/cn/hwyee/Main.java
@@ -17,9 +17,17 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!\n");
+        /**
+         * 发布者
+         */
         SubmissionPublisher<String> publisher = new SubmissionPublisher<String>();
+
+        /**
+         * 订阅者
+         */
         Flow.Subscriber subscriber = new Flow.Subscriber() {
-            Flow.Subscription subscription ;
+            Flow.Subscription subscription;
+
             /**
              * onSubscribe:
              * 订阅时触发
@@ -31,9 +39,9 @@ public class Main {
              */
 
             @Override
-            public void onSubscribe(Flow.Subscription subscription)  {
+            public void onSubscribe(Flow.Subscription subscription) {
                 this.subscription = subscription;
-                System.out.println(Thread.currentThread().getName() +":Onsubscribed.");
+                System.out.println(Thread.currentThread().getName() + ":Subscriber Onsubscribed.");
 
                 subscription.request(1);
             }
@@ -49,33 +57,90 @@ public class Main {
              */
             @Override
             public void onNext(Object item) {
-                System.out.println(Thread.currentThread().getName() +":onNext.");
+                System.out.println(Thread.currentThread().getName() + ":" + item + "->Subscriber onNext.");
                 subscription.request(1);
             }
 
             @Override
             public void onError(Throwable throwable) {
-                System.out.println(Thread.currentThread().getName() +":onError.");
+                System.out.println(Thread.currentThread().getName() + ":Subscriber onError.");
             }
 
             @Override
             public void onComplete() {
-                System.out.println(Thread.currentThread().getName() +":onComplete.");
+                System.out.println(Thread.currentThread().getName() + ":Subscriber onComplete.");
             }
         };
-        publisher.subscribe(subscriber);
+        Flow.Processor processor1 = new MyProcessor(1);
+        Flow.Processor processor2 = new MyProcessor(2);
+        Flow.Processor processor3 = new MyProcessor(3);
+        //绑定订阅关系
+        publisher.subscribe(processor1);
+        processor1.subscribe(processor2);
+        processor2.subscribe(processor3);
+        processor3.subscribe(subscriber);
+
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println(Thread.currentThread().getName() + ":i = " + i);
-            publisher.submit("i-"+i);
+            publisher.submit("i-" + i);
             if (i == 3) {
-                publisher.closeExceptionally(new RuntimeException("publish error."));
+                publisher.submit("i-" + i + 3000);
+//                publisher.closeExceptionally(new RuntimeException("publish error."));
             }
         }
         Thread.sleep(10000);
 
+    }
+}
+
+/**
+ * Processor:
+ * Processor 可以既是发布者，又是消费者。
+ *
+ * @author hui
+ * @version 1.0
+ * @return
+ * @date 2024/1/28 14:55
+ */
+class MyProcessor extends SubmissionPublisher<String> implements Flow.Processor<String, String> {
+    int num;
+
+    public MyProcessor() {
+    }
+
+    public MyProcessor(int i) {
+        num = i;
+    }
+
+    Flow.Subscription subscription;
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        this.subscription = subscription;
+        System.out.println(Thread.currentThread().getName() + "Processor" + num + " onSubscribe.");
+        subscription.request(1);
+    }
+
+    @Override
+    public void onNext(String item) {
+        System.out.println(Thread.currentThread().getName() + "Processor" + num + " onNext.");
+        item += "->processor" + num;
+        subscription.request(1);
+        //处理完后可以再把消息发布出去
+        submit(item);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        System.out.println(Thread.currentThread().getName() + "Processor" + num + " onNext.");
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println(Thread.currentThread().getName() + "Processor" + num + " onNext.");
     }
 }
