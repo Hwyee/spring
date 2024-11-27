4ee334862eb0578a76d5c580e877f6d66a72ab6e
hwyee <hwyee@foxmail.com>
2024-11-27 17:54:02 +0800

    tomcat deploy springboot.

--- security/src/main/java/cn/hwyee/security/SecurityApplication.java
+++ security/src/main/java/cn/hwyee/security/SecurityApplication.java
@@ -2,12 +2,18 @@ package cn.hwyee.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SecurityApplication.class);
+    }
 }
